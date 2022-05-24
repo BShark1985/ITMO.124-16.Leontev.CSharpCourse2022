d9ad84a69f4a75cd096045b291e81f2f963aceb8
BShark1985 <leonand1985@gmail.com>
2022-05-24 20:10:16 +0300

    Added Lab 4,5,6

---
+++ LAB_04-01/Test.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LAB_04_01
+{
+    internal class Test
+    {
+        static void Main(string[] args)
+        {
+            int x;
+            int y;
+            int greater;
+            Console.WriteLine("Please, enter first number");
+            x = int.Parse(Console.ReadLine());
+            Console.WriteLine("Please, enter second number");
+            y = int.Parse(Console.ReadLine());
+            greater = Utils.Greater(x, y);
+            Console.WriteLine("The greater value is " + greater);
+        }
+    }
+}
---
+++ LAB_04-01/Utils.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LAB_04_01
+{
+    internal class Utils
+    {
+        public static int Greater(int a, int b)
+        {
+            if (a > b)
+                return a;
+            else
+                return b;
+        }
+    }
+}
